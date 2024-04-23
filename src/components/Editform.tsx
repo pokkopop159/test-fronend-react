b4185d6c4ca065a0da5e2236c367a152df6e37b2
pokkopop159 <poknaphat@gmail.com>
2024-04-23 13:10:03 +0700

    update

--- src/components/Editform.tsx
+++ src/components/Editform.tsx
@@ -16,7 +16,8 @@ const Editform = ({isEditProps, setIsEditProps, keyProps}: EditProps) => {
     const appReducer = useSelector(appSelector)
     const { t } = useTranslation()
     const data = appReducer.data
-
+    const Item: any = data.find((item: any) => item.key === keyProps)
+    console.log(Item)
     const Nationality = [
         {
           value: "British / English",
@@ -47,49 +48,48 @@ const Editform = ({isEditProps, setIsEditProps, keyProps}: EditProps) => {
       }
     return (
         <div className='Flexbox'>
-            <Form form={form} name='dataForm' onFinish={onFinish} >
-                <Form.Item label={t('Name Titles')} name='tname' >
-                    <Select
-                        options={[
-                            { value: "Mr.", label: "Mr." },
-                            { value: "Mrs.", label: "Mrs." },
-                            { value: "Ms.", label: "Ms." },
-                        ]}
-                    />
+           <Form form={form} name='dataForm' onFinish={onFinish} >
+                    <Form.Item label={t('Name Titles')} name='tname' >
+                        <Select
+                            options={[
+                                { value: "Mr.", label: "Mr." },
+                                { value: "Mrs.", label: "Mrs." },
+                                { value: "Ms.", label: "Ms." },
+                            ]}
+                        />
+                    </Form.Item>
+                    <Form.Item label={t("First Name")} name='fname'  >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t("Last Name")} name='lname'  >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t('Date')} name="date" >
+                        <DatePicker placeholder={t('MM/DD/YYYY')} format='MM/DD/YYYY/' />
+                    </Form.Item>
+                    <Form.Item label={t('Nationality')} name='nationality'  >
+                        <Select options={Nationality} />
+                    </Form.Item>
+                    <Form.Item label={t('ID Card')} name='idcard'>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t('Gender')} name='gender' >
+                        <Radio.Group options={[t('Male'), t('Female'), t('Not specified')]} />
+                    </Form.Item>
+                    <Form.Item label={t('Phone')} name='phone'  >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t('Passport')} name='passport' >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t('Expected Salary')} name='salary'  >
+                        <Input type='number' />
+                    </Form.Item>
+                <Form.Item>
+                  <Button type="default" onClick={onCancel}>{t("Cancel")} </Button>
+                  <Button type='primary' htmlType='submit' >{t("Update")}</Button>
                 </Form.Item>
-                <Form.Item label={t("First Name")} name='fname'  >
-                    <Input />
-                </Form.Item>
-                <Form.Item label={t("Last Name")} name='lname'  >
-                    <Input />
-                </Form.Item>
-                <Form.Item label={t('Date')} name="date" >
-                    <DatePicker placeholder={t('MM/DD/YYYY')} format='MM/DD/YYYY/' />
-                </Form.Item>
-                <Form.Item label={t('Nationality')} name='nationality'  >
-                    <Select options={Nationality} />
-                </Form.Item>
-                <Form.Item label={t('ID Card')} name='idcard'>
-                    <Input />
-                </Form.Item>
-                <Form.Item label={t('Gender')} name='gender' >
-                    <Radio.Group options={[t('Male'), t('Female'), t('Not specified')]} />
-                </Form.Item>
-                <Form.Item label={t('Phone')} name='phone'  >
-                    <Input />
-                </Form.Item>
-                <Form.Item label={t('Passport')} name='passport' >
-                    <Input />
-                </Form.Item>
-                <Form.Item label={t('Expected Salary')} name='salary'  >
-                    <Input type='number' />
-                </Form.Item>
-         
-            <Form.Item>
-              <Button type="default" onClick={onCancel}>{t("Cancel")} </Button>
-              <Button type='primary' htmlType='submit' >{t("Update")}</Button>
-            </Form.Item>
-            </Form>
+                </Form>
         </div>
     )
 }
